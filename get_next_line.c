8e984c99ca37fe5ee9cd15c510a2d794f9d6da2f
Theo Martin <theomart@z4r11p2.42lyon.fr>
2025-11-28 16:31:34 +0100

    get next line

---
+++ get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: theomart <theomart@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 11:23:46 by theomart          #+#    #+#             */
+/*   Updated: 2025/11/28 15:43:38 by theomart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+
+	__builtin_printf("%d", fd);
+	return (NULL);
+}
