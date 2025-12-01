33017d0ae9e3b40db92d9541e191b502056a40d7
Theo Martin <theomart@z4r12p1.42lyon.fr>
2025-12-01 12:45:42 +0100

    dsdsd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: theomart <theomart@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 11:23:46 by theomart          #+#    #+#             */
-/*   Updated: 2025/12/01 11:46:06 by theomart         ###   ########.fr       */
+/*   Updated: 2025/12/01 12:45:25 by theomart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer;
+	char		*line;
 
-	while (buffer[i] != '\n' || buffer[i] != '\0')
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+}
+
+char	*fill_line(char *f_line, int fd, char *buffer)
+{
+	size_t	fread;
+	char	*tmp;
+
+	fread = read(fd, buffer, BUFFER_SIZE);
+	while (fread > 0)
 	{
 	}
+	return (f_line)
 }
